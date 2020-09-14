9ea8907c24ecc4047fbc7553f11f3dca859ba059
LanderVanlaer <lander.vanlaer1@gmail.com>
2020-09-14 11:15:22 +0200

    update oef 2

--- src/me/landervanlaer/javaFx/oef2/Controller.java
+++ src/me/landervanlaer/javaFx/oef2/Controller.java
@@ -1,6 +1,7 @@
 package me.landervanlaer.javaFx.oef2;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
@@ -9,9 +10,19 @@ public class Controller {
     public Label label;
     public TextField firstName;
     public TextField lastName;
+    public Label warning;
 
     public void handleButtonAction(ActionEvent event) {
-        label.getText();
-    }
+        warning.setText("");
+        label.setText("");
 
+        if(firstName.getText().isEmpty())
+            warning.setText("Gelieve een voornaam in te geven");
+        else if(lastName.getText().isEmpty())
+            warning.setText("Gelieve een achternaam in te geven");
+        else
+            label.setText(firstName.getText().toLowerCase().charAt(0) +
+                    lastName.getText().toLowerCase().replaceAll(" ", "")
+                            .substring(0, Math.min(lastName.getText().length(), 6)));
+    }
 }
