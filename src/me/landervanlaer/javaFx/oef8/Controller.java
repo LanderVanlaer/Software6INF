3c17abc538f58ffee4fc6649af0a8b016e4a0f04
LanderVanlaer <lander.vanlaer1@gmail.com>
2020-09-25 08:37:10 +0200

    Update Oef8, mogelijkheid om bestand aan te maken nog toevoegen

--- src/me/landervanlaer/javaFx/oef8/Controller.java
+++ src/me/landervanlaer/javaFx/oef8/Controller.java
@@ -1,6 +1,7 @@
 package me.landervanlaer.javaFx.oef8;
 
 import com.sun.javafx.scene.control.DoubleField;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
@@ -8,20 +9,36 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Controller {
-    public TableView<Leerling> table;
-    public TextField naam;
-    public TextField punten;
+    public TableView<Punten> table;
     public Label titelNaam;
 
+    public TextField naam;
+    public TextField punten;
+    public TextField vak;
+
     @FXML
-    private TableColumn<Leerling, String> naamKolom;
+    private TableColumn<Punten, String> naamKolom;
     @FXML
-    private TableColumn<Leerling, Double> gemiddeldeKolom;
+    private TableColumn<Punten, Double> puntenKolom;
+
+    private Leerling leerling;
 
     public void initialize() {
-        naamKolom.setCellValueFactory(new PropertyValueFactory<>("naam"));
-        gemiddeldeKolom.setCellValueFactory(new PropertyValueFactory<>("gemiddelde"));
+        naamKolom.setCellValueFactory(new PropertyValueFactory<>("vak"));
+        puntenKolom.setCellValueFactory(new PropertyValueFactory<>("punt"));
+    }
+
+    public void changeLeerling(ActionEvent actionEvent) {
+        table.getItems().clear();
+
+        Leerling leerling = new Leerling(this.naam.getText().toLowerCase());
+        this.titelNaam.setText(leerling.getNaam());
+        leerling.addToTable(table);
     }
 }
 
--- src/me/landervanlaer/javaFx/oef8/Leerling.java
+++ src/me/landervanlaer/javaFx/oef8/Leerling.java
@@ -1,13 +1,43 @@
 package me.landervanlaer.javaFx.oef8;
 
-import java.io.File;
+import javafx.scene.control.TableView;
 
-public class Leerling extends me.landervanlaer.javaFx.oef7.Leerling {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Leerling {
+    private final String naam;
     private File bestand;
 
-    public Leerling(String naam, double gemiddelde) {
-        super(naam, gemiddelde);
-        this.setBestand(new File("src/resources/puntenOef8/" + this.getNaam() + ".txt"));
+    public Leerling(String naam) {
+        this.naam = naam;
+        this.bestand = new File("src/resources/puntenOef8/punten_" + this.getNaam() + ".txt");
+    }
+
+    public void addToTable(TableView<Punten> table) {
+        try {
+            Scanner scan = new Scanner(this.getBestand());
+            scan.nextLine();
+            while(scan.hasNextLine()) {
+                final String line = scan.nextLine();
+                final String vakNaam = line.split(": ")[0];
+                final double vakPunt = Double.parseDouble(line.split(": ")[1]);
+
+                table.getItems().add(new Punten(vakNaam, vakPunt));
+            }
+        } catch(FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean canUseBestand() {
+        return this.getBestand().isFile() && this.getBestand().exists() && this.getBestand().canRead();
+    }
+
+
+    public String getNaam() {
+        return naam;
     }
 
     public File getBestand() {
