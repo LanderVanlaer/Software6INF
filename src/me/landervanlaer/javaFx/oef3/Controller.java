c1054447b2cd969dac1ff1ff624e98bb70d8ea05
Lander Van laer <lander.vanlaer1@gmail.com>
2020-09-29 20:13:37 +0200

    Fix oef3: controller ChoiseBox generic naar Integer

--- src/me/landervanlaer/javaFx/oef3/Controller.java
+++ src/me/landervanlaer/javaFx/oef3/Controller.java
@@ -8,14 +8,14 @@ import javafx.scene.control.TextField;
 
 public class Controller {
 
-    public ChoiceBox choiseBox;
+    public ChoiceBox<Integer> choiseBox;
     public Label label;
 
     static final int min = 48;
     static final int max = 90;
 
     public void handleButtonAction(ActionEvent event) {
-        if(!(choiseBox.getValue() instanceof Integer)) throw new Error("Value is not an instance of Integer");
+        if(choiseBox.getValue() == null) throw new Error("Value is not an instance of Integer");
         StringBuilder stringBuilder = new StringBuilder();
         for(int i = 0; i < (Integer) choiseBox.getValue(); i++)
             stringBuilder.append((char) (Math.random() * (max - min) + min));
