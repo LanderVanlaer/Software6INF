4208a9d9ee896c1dfc24b1ebfb68a1eddd8f2ee8
Lander Van laer <lander.vanlaer1@gmail.com>
2020-10-08 19:48:50 +0200

    Change oef 11, les 08102020

--- src/me/landervanlaer/javaFx/oef11/Build.java
+++ src/me/landervanlaer/javaFx/oef11/Build.java
@@ -4,29 +4,11 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
+import java.util.ArrayList;
+
 public class Build {
     public TextField naam;
-    public GridPane gridUpperSection;
-    public GridPane totalUpperSection;
-    public GridPane gridLowerSection;
-    private Label[][] labels;
 
     public void initialize() {
-        setLabels(new Label[6][6]);
-        for(int i = 0; i < gridUpperSection.getColumnCount(); i++) {
-            for(int j = 0; j < gridUpperSection.getRowCount(); j++) {
-                Label label = new Label(i + ", " + j);
-                getLabels()[i][j] = label;
-                gridUpperSection.add(label, i, j);
-            }
-        }
-    }
-
-    public Label[][] getLabels() {
-        return labels;
-    }
-
-    public void setLabels(Label[][] labels) {
-        this.labels = labels;
     }
 }
